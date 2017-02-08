6ea514caa84b91d7eaeae57dba398b6dc3699474
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-02-08 18:38:45 +0100

    fetch data and display

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Table, Column, Cell} from 'fixed-data-table';
+import 'fixed-data-table/dist/fixed-data-table.css'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books: []
+    };
+  }
+
+  componentDidMount() {
+    let books;
+    fetch('http://localhost:3000/clippings.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({books: responseJson})
+        books = responseJson;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
+    let highlight = this.state.books[0];
     return (
+      <div>
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Nazwa ksiązki: {highlight && highlight.book}<br/>
+        Data utworzenia: {highlight && highlight.creation_date}<br/>
+        Cytat: {highlight && highlight.body}<br/>
       </div>
+
+      <Table
+              rowsCount={100}
+              rowHeight={50}
+              width={1000}
+              height={500}>
+              <Column
+                cell={<Cell>Basic content</Cell>}
+                width={200}
+              />
+            </Table>
+        </div>
     );
   }
 }
